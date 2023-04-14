770aafe4856f796753490364d8173b4bcba316b1
FranciscoGontijo <franciscoacmg@gmail.com>
2023-04-14 14:18:00 +1000

    Forgot to add the page file (Homepage)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,46 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 
-//import components
-import NavBar from '../../components/Navbar';
+//import images;
 
+
+//import components
+import MainBanner from '../../components/MainBanner';
+import HighlightsMobileHomepage from '../../components/HighlightsMobileHomepage';
+import HighlightsLaptopHomepage from '../../components/HighlightsLaptopHomepage';
+
+//import utils
+import useWindowSize from '../../util/useWindowSize';
 
 const Home: NextPage = () => {
+  const screenSize: {width: number | undefined; height: number | undefined} = useWindowSize();
+  const [display, setDisplay] = useState<string>('laptop');
+
+  useEffect(() => { 
+    if (screenSize.width === undefined) {
+      return
+    }
+    if (screenSize.width > 700) {
+      setDisplay('laptop');
+    }
+    if (screenSize.width <= 700) {
+      setDisplay('mobile');
+    }
+  }, [screenSize]);
+
   return (
-    <div>
+    <main className={styles.homepage}>
       <Head>
-        <title>Home</title>
+        <title>Austral Store</title>
       </Head>
-      <NavBar/>
-      <h1>Hello world</h1>
-    </div>
+      <MainBanner />
+      {display === 'mobile' && <HighlightsMobileHomepage />}
+      {display === 'laptop' && <HighlightsLaptopHomepage />}
+    </main>
   )
 };
 
