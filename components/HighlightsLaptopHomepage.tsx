09e50a3e76e8ca951fc24199cb06b7be3ae0a269
FranciscoGontijo <franciscoacmg@gmail.com>
2023-04-15 15:38:57 +1000

    Highlights section cleaning code and using map to display the images

--- components/HighlightsLaptopHomepage.tsx
+++ components/HighlightsLaptopHomepage.tsx
@@ -13,64 +13,32 @@ import highlightsSecondImageSrc from '../src/assets/images/homepage/Highlights T
 import highlightsThirdImageSrc from '../src/assets/images/homepage/Highlights Jackets.webp';
 import highlightsFourthImageSrc from '../src/assets/images/homepage/Highlights Sales.webp';
 
+type ProductType = {
+    imageSrc: any;
+    pageHref: string;
+    descriptionTitle: string;
+    description: string
+};
+
+const productsArray: ProductType[] = [
+    { imageSrc: highlightsFirstImageSrc, pageHref: '/store', descriptionTitle: 'Winter 23', description: 'Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.' },
+    { imageSrc: highlightsSecondImageSrc, pageHref: '/store/t-shirts', descriptionTitle: 'T-shirts', description: 'Explore as clássicas camisetas da Austral. Produzidas nacionalmente, com materiais da mais alta qualidade disponível no mercado nacional. O algodão utilizado possui certificação BCI®, garantindo baixo impacto ao meio ambiente.' },
+    { imageSrc: highlightsThirdImageSrc, pageHref: '/store/jackets', descriptionTitle: 'Jackets', description: 'Os casacos da Austral, além de se adaptarem a diferentes temperaturas e ocasiões, também são produzidos com tecidos tecnológicos e sustentáveis, que garantem durabilidade e estilo ao mesmo tempo.' },
+    { imageSrc: highlightsFourthImageSrc, pageHref: '/salespage', descriptionTitle: 'Sales', description: 'Peças selecionadas com descontos. As promoções da Austral acontecem por tempo limitado. Explore os produtos com oportunidades de compra especiais. Sugerimos o uso de filtros de tamanho para uma melhor navegação.' },
+]
+
 
 //import CSS
 import styles from "../src/styles/HighlightsLaptop.module.css";
 
-//set 2 diferents state ish
-
-
 
 const HighlightsLaptopHomepage = () => {
-    const [container, setContainer] = useState<string>('first container');
-
-    const [firstImageSrc, setFirstImageSrc] = useState(highlightsFirstImageSrc);
-    const [firstPageHref, setFirstPageHref] = useState<string>('/store')
-    const [firstDescriptionTitle, setFirstDescriptionTitle] = useState<string>('Winter 23');
-    const [firstDescription, setFirstDescription] = useState<string>('Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.')
-    
-    const [secondImageSrc, setSecondImageSrc] = useState(highlightsSecondImageSrc);
-    const [secondPageHref, setSecondPageHref] = useState<string>('/store/t-shirts')
-    const [secondDescriptionTitle, setSecondDescriptionTitle] = useState<string>('T-shirts');
-    const [secondDescription, setSecondDescription] = useState<string>('Explore as clássicas camisetas da Austral. Produzidas nacionalmente, com materiais da mais alta qualidade disponível no mercado nacional. O algodão utilizado possui certificação BCI®, garantindo baixo impacto ao meio ambiente.')
-
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
 
     const nextContainer = (): void => {
-        if (container === 'first container') {
-            //First Image
-            setFirstImageSrc(highlightsThirdImageSrc);
-            setFirstPageHref('/store/jackets');
-            setFirstDescriptionTitle('Jackets');
-            setFirstDescription('Os casacos da Austral, além de se adaptarem a diferentes temperaturas e ocasiões, também são produzidos com tecidos tecnológicos e sustentáveis, que garantem durabilidade e estilo ao mesmo tempo.');
-
-            //Second Image
-            setSecondImageSrc(highlightsFourthImageSrc);
-            setSecondPageHref('/salespage');
-            setSecondDescriptionTitle('Sales');
-            setSecondDescription('Peças selecionadas com descontos. As promoções da Austral acontecem por tempo limitado. Explore os produtos com oportunidades de compra especiais. Sugerimos o uso de filtros de tamanho para uma melhor navegação.');
-            
-            //Set Container
-            setContainer('second container');
-            return
-        }
-        if (container === 'second container') {
-            //First Image
-            setFirstImageSrc(highlightsFirstImageSrc);
-            setFirstPageHref('/store');
-            setFirstDescriptionTitle('Winter 23');
-            setFirstDescription('Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.');
-
-            //Second Image
-            setSecondImageSrc(highlightsSecondImageSrc);
-            setSecondPageHref('/salespage');
-            setSecondDescriptionTitle('Sales');
-            setSecondDescription('Explore as clássicas camisetas da Austral. Produzidas nacionalmente, com materiais da mais alta qualidade disponível no mercado nacional. O algodão utilizado possui certificação BCI®, garantindo baixo impacto ao meio ambiente.');
-            
-            //Set Container 
-            setContainer('first container');
-            return
-        }
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? 2 : 0);
     }
+
 
     return (
         <section className={styles.highlights_container}>
@@ -78,44 +46,31 @@ const HighlightsLaptopHomepage = () => {
                 <h3>Highlights</h3>
                 <h2>Explore categories</h2>
             </div>
+            
             <div className={styles.all_categories_container}>
-                <IoIosArrowBack className={styles.back_arrow} />
+                <IoIosArrowBack onClick={nextContainer} className={styles.back_arrow} />
                 <IoIosArrowForward onClick={nextContainer} className={styles.foward_arrow} />
-                <div className={styles.categorie_container}>
-                    <div className={styles.images_container}>
-                        <Link href={firstPageHref}>
-                            <Image
-                                src={firstImageSrc}
-                                alt="Example Image"
-                                className={styles.image}
-                            />
-                        </Link>
-                    </div>
-                    <div className={styles.description_container}>
-                        <h3>{firstDescriptionTitle}</h3>
-                        <p>{firstDescription}</p>
-                        <Link className={styles.see_more_link} href={firstPageHref}>See More</Link>
-                    </div>
-                </div>
-                <div className={styles.categorie_container}>
-                    <div className={styles.images_container}>
-
-                        <Link href={secondPageHref}>
-                            <Image
-                                src={secondImageSrc}
-                                alt="Example Image"
-                                className={styles.image}
-                            />
-                        </Link>
-                    </div>
-                    <div className={styles.description_container}>
-                        <h3>{secondDescriptionTitle}</h3>
-                        <p>{secondDescription}</p>
-                        <Link className={styles.see_more_link} href={secondPageHref}>See More</Link>
-                    </div>
-                </div>
+                {productsArray.slice(currentIndex, currentIndex + 2).map((product) => {
+                    return (
+                        <div className={styles.categorie_container}>
+                            <div className={styles.image_container}>
+                                <Link className={styles.link} href={product.pageHref}>
+                                    <Image
+                                        src={product.imageSrc}
+                                        alt='test image'
+                                        className={styles.image}
+                                    />
+                                </Link>
+                            </div>
+                            <div className={styles.description_container}>
+                            <h3>{product.descriptionTitle}</h3>
+                            <p>{product.description}</p>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
-        </section>
+        </section >
     )
 };
 
--- components/HighlightsMobileHomepage.tsx
+++ components/HighlightsMobileHomepage.tsx
@@ -4,6 +4,10 @@ import { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 
+//import CSS
+import styles from "../src/styles/HighlightsMobile.module.css";
+
+
 //import icons from react-icons
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
@@ -14,41 +18,40 @@ import highlightsThirdImageSrc from '../src/assets/images/homepage/Highlights Ja
 import highlightsFourthImageSrc from '../src/assets/images/homepage/Highlights Sales.webp';
 
 
-//import CSS
-import styles from "../src/styles/HighlightsMobile.module.css";
 
+type CategorieType = {
+    imageSrc: any;
+    pageHref: string;
+    descriptionTitle: string;
+    description: string
+};
+
+const categoriesArray: CategorieType[] = [
+    { imageSrc: highlightsFirstImageSrc, pageHref: '/store', descriptionTitle: 'Winter 23', description: 'Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.' },
+    { imageSrc: highlightsSecondImageSrc, pageHref: '/store/t-shirts', descriptionTitle: 'T-shirts', description: 'Explore as clássicas camisetas da Austral. Produzidas nacionalmente, com materiais da mais alta qualidade disponível no mercado nacional. O algodão utilizado possui certificação BCI®, garantindo baixo impacto ao meio ambiente.' },
+    { imageSrc: highlightsThirdImageSrc, pageHref: '/store/jackets', descriptionTitle: 'Jackets', description: 'Os casacos da Austral, além de se adaptarem a diferentes temperaturas e ocasiões, também são produzidos com tecidos tecnológicos e sustentáveis, que garantem durabilidade e estilo ao mesmo tempo.' },
+    { imageSrc: highlightsFourthImageSrc, pageHref: '/salespage', descriptionTitle: 'Sales', description: 'Peças selecionadas com descontos. As promoções da Austral acontecem por tempo limitado. Explore os produtos com oportunidades de compra especiais. Sugerimos o uso de filtros de tamanho para uma melhor navegação.' },
+]
 
 const HighlightsMobileHomepage = () => {
-    const [imageSrc, setImageSrc] = useState(highlightsFirstImageSrc);
-    const [pageHref, setPageHref] = useState('/store')
-    const [descriptionTitle, setDescriptionTitle] = useState('Winter 23');
-    const [description, setDescription] = useState('Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.')
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-    //    const nextImage = (imageSrc, highlightsFirstImageSrc, highlightsSecondImageSrc, highlightsThirdImageSrc, highlightsFourthImageSrc, setImageSrc, setPageHref, setDescriptionTitle, setDescription) => {
-    const nextImage = () => {
-        if (imageSrc === highlightsFirstImageSrc) {
-            setImageSrc(highlightsSecondImageSrc);
-            setPageHref('/store/t-shirts');
-            setDescriptionTitle('T-shirts');
-            setDescription('Explore as clássicas camisetas da Austral. Produzidas nacionalmente, com materiais da mais alta qualidade disponível no mercado nacional. O algodão utilizado possui certificação BCI®, garantindo baixo impacto ao meio ambiente.');
-        } else if (imageSrc === highlightsSecondImageSrc) {
-            setImageSrc(highlightsThirdImageSrc);
-            setPageHref('/store/jackets');
-            setDescriptionTitle('Jackets');
-            setDescription('Os casacos da Austral, além de se adaptarem a diferentes temperaturas e ocasiões, também são produzidos com tecidos tecnológicos e sustentáveis, que garantem durabilidade e estilo ao mesmo tempo.');
-        } else if (imageSrc === highlightsThirdImageSrc) {
-            setImageSrc(highlightsFourthImageSrc);
-            setPageHref('/salespage');
-            setDescriptionTitle('Sales');
-            setDescription('Peças selecionadas com descontos. As promoções da Austral acontecem por tempo limitado. Explore os produtos com oportunidades de compra especiais. Sugerimos o uso de filtros de tamanho para uma melhor navegação.');
+    const nextImage = (): void => {
+        if (currentIndex === categoriesArray.length - 1) {
+            setCurrentIndex((prevIndex) => prevIndex = 0);
+            return
         }
-        else if (imageSrc === highlightsFourthImageSrc) {
-            setImageSrc(highlightsFirstImageSrc);
-            setPageHref('/store');
-            setDescriptionTitle('Winter 21');
-            setDescription('Tomorrow não é apenas sobre o futuro, mas também um lembrete de que o presente é o momento de escolher novos caminhos.');
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+
+    const backImage = (): void => {
+        if (currentIndex === 0) {
+            setCurrentIndex((prevIndex) => prevIndex = 3);
+            return
         }
-    };
+        setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+
 
     return (
         <section className={styles.highlights_container}>
@@ -57,24 +60,26 @@ const HighlightsMobileHomepage = () => {
                 <h2>Explore categories</h2>
             </div>
             <div className={styles.all_categories_container}>
+                <IoIosArrowBack onClick={backImage} className={styles.back_arrow} />
+                <IoIosArrowForward onClick={nextImage} className={styles.foward_arrow} />
+
                 <div className={styles.categorie_container}>
                     <div className={styles.images_container}>
-                        <IoIosArrowBack className={styles.back_arrow} />
-                        <IoIosArrowForward onClick={nextImage} className={styles.foward_arrow} />
-                        <Link href={pageHref}>
+                        <Link href={categoriesArray[currentIndex].pageHref}>
                             <Image
-                                src={imageSrc}
+                                src={categoriesArray[currentIndex].imageSrc}
                                 alt="Example Image"
                                 className={styles.image}
                             />
                         </Link>
                     </div>
                     <div className={styles.description_container}>
-                        <h3>{descriptionTitle}</h3>
-                        <p>{description}</p>
-                        <Link className={styles.see_more_link} href={pageHref}>See More</Link>
+                        <h3>{categoriesArray[currentIndex].descriptionTitle}</h3>
+                        <p>{categoriesArray[currentIndex].description}</p>
+                        <Link className={styles.see_more_link} href={categoriesArray[currentIndex].pageHref}>See More</Link>
                     </div>
                 </div>
+
             </div>
         </section>
     )
