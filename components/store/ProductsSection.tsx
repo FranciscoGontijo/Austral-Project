555998e6b885d7550c160b2060b3c6837f349907
FranciscoGontijo <franciscoacmg@gmail.com>
2023-04-19 09:26:11 +1000

    Adding Filter component to ProductsSection to appears when screen width is bigger than 1000px

--- components/store/ProductsSection.tsx
+++ components/store/ProductsSection.tsx
@@ -1,5 +1,10 @@
+//import react hooks
 import { useState, useEffect } from 'react';
 
+//import components
+import FilterComponent from './FilterComponent';
+
+//import next components
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -41,7 +46,8 @@ const ProductsSection = () => {
             setFilteredProductsArray(array => [...array].sort((productA, productB) => (productA.price > productB.price ? 1 : -1)));
         }
         if (sortProducts === 'Price max to min') {
-            setFilteredProductsArray(array => [...array].sort((productA, productB) => (productA.price < productB.price ? 1 : -1)));        }
+            setFilteredProductsArray(array => [...array].sort((productA, productB) => (productA.price < productB.price ? 1 : -1)));
+        }
     };
 
     useEffect(() => {
@@ -49,45 +55,52 @@ const ProductsSection = () => {
     }, [sortProducts]);
 
 
-
     return (
         <section className={styles.products_section_container}>
-            <select
-                value={categorie}
-                onChange={(e) => setCategorie(e.target.value)}>
-                <option value="" disabled>Filter per categorie</option>
-                <option value='All'>All</option>
-                <option value='Hoodie'>Hoodie</option>
-                <option value='T-shirt'>T-Shirt</option>
-                <option value='Sweatshirt'>Sweatshirt</option>
-                <option value='Shirt'>Shirt</option>
-                <option value='Swimshorts'>Swimshorts</option>
-            </select>
-            <select
-                value={sortProducts}
-                onChange={(e) => setSortProducts(e.target.value)}>
-                <option value="" disabled>Sort by</option>
-                <option value='Price min to max'>Price (min to max)</option>
-                <option value='Price max to min'>Price (max to min)</option>
-            </select>
-            <div className={styles.all_products_container}>
-                {filteredProductsArray.map((product) => {
-                    return (
-                        <div key={product.key} className={styles.product_container}>
-                            <Link className={styles.link} href={product.pageHref}>
-                                <div className={styles.image_container}>
-                                    <Image
-                                        src={product.imageSrc}
-                                        alt='test image'
-                                        className={styles.image}
-                                    />
-                                </div>
-                            </Link>
-                            <h3>{product.name}</h3>
-                            <h2>AU$ {product.price}.00</h2>
-                        </div>
-                    )
-                })}
+            <div className={styles.buttons_container}>
+                <select
+                    className={styles.filter_button}
+                    value={categorie}
+                    onChange={(e) => setCategorie(e.target.value)}>
+                    <option value="" disabled>Filter per categorie</option>
+                    <option value='All'>All</option>
+                    <option value='Hoodie'>Hoodie</option>
+                    <option value='T-shirt'>T-Shirt</option>
+                    <option value='Sweatshirt'>Sweatshirt</option>
+                    <option value='Shirt'>Shirt</option>
+                    <option value='Swimshorts'>Swimshorts</option>
+                </select>
+                <p className={styles.product_quantity_text}>{`${filteredProductsArray.length} Products`}</p>
+                <select
+                    className={styles.sort_button}
+                    value={sortProducts}
+                    onChange={(e) => setSortProducts(e.target.value)}>
+                    <option value="" disabled>Sort by</option>
+                    <option value='Price min to max'>Price (min to max)</option>
+                    <option value='Price max to min'>Price (max to min)</option>
+                </select>
+            </div>
+            <div className={styles.big_screen_products_container}>
+                <FilterComponent />
+                <div className={styles.all_products_container}>
+                    {filteredProductsArray.map((product) => {
+                        return (
+                            <div key={product.key} className={styles.product_container}>
+                                <Link className={styles.link} href={product.pageHref}>
+                                    <div className={styles.image_container}>
+                                        <Image
+                                            src={product.imageSrc}
+                                            alt='test image'
+                                            className={styles.image}
+                                        />
+                                    </div>
+                                </Link>
+                                <h3>{product.name}</h3>
+                                <h2>AU$ {product.price}.00</h2>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </section>
     )
