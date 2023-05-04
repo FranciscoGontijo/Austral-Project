a667d98a471904abd8e6bafa95c761641bc56367
FranciscoGontijo <franciscoacmg@gmail.com>
2023-05-04 20:01:27 +1000

    Change the displayed message when send the form at contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 
+//import react icons
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+
 //import CSS
 import styles from "../../src/styles/contactpage/ContactPage.module.css"
 
@@ -7,19 +10,23 @@ const ContactPage = () => {
     const [display, setDisplay] = useState('form');
 
     return (
-        <section className={styles.contact_page}>   
+        <section className={styles.contact_page}>
             {display === 'form' && <div className={styles.form_container}>
                 <h2>Get in touch</h2>
-                <form onSubmit={() => setDisplay('message')}  className={styles.contact_form}>
-                    <input type="text"  className={styles.name_input} placeholder="Name" name="user_name" required />
-                    <input type="email"  className={styles.email_input} placeholder="Email" name="user_email" required />
+                <form onSubmit={() => setDisplay('message')} className={styles.contact_form}>
+                    <input type="text" className={styles.name_input} placeholder="Name" name="user_name" required />
+                    <input type="email" className={styles.email_input} placeholder="Email" name="user_email" required />
                     <textarea className={styles.message_input} placeholder="Message" name="message" required />
-                    <button type="submit" value="Send"  className={styles.form_send_button}>SEND MESSAGE</button>
+                    <button type="submit" value="Send" className={styles.form_send_button}>SEND MESSAGE</button>
                 </form>
             </div>}
             {display === 'message' && <div className={styles.sended_message_section}>
                 <h1>Message sent successfully</h1>
-                <p>Thank you for getting in touch, as soon as possible I will respond to your message. Cheers</p>
+                <p>This is just a simulation, if you want to get in touch with the developer use this <a href="https://www.linkedin.com/in/francisco-gontijo-5a469b239/" target="_blank">link</a> or send an email to franciscoacmg@gmail.com</p>
+                <div>
+                    <FaGithub className={styles.social_icons}/>
+                    <FaLinkedinIn className={styles.social_icons}/>
+                </div>
                 <button onClick={() => setDisplay('form')}>Back</button>
             </div>}
         </section>
